e42b802682f721f2d682f036082885f20d4e94d1
Harold <haroldcj95@gmail.com>
2019-02-19 09:12:35 -0400

    beautify code

--- TennisGame2.js
+++ TennisGame2.js
@@ -1,4 +1,4 @@
-var TennisGame2 = function(player1Name, player2Name) {
+var TennisGame2 = function (player1Name, player2Name) {
     this.Player1CurrentPoints = 0;
     this.Player2CurrentPoints = 0;
 
@@ -9,164 +9,146 @@ var TennisGame2 = function(player1Name, player2Name) {
     this.player2Name = player2Name;
 };
 
-TennisGame2.prototype.calculateCurrentGameScore = function() {
+TennisGame2.prototype.calculateCurrentGameScore = function () {
     let score = "";
-    if(this.checkIfAnyPlayerHasWon()){
-        score=this.calculateWhichPlayerWon();
-    }
-    else{
-        if(this.checkIfPlayerPointsAreTied()){
-            score=this.calculateScoreForTiedPoints();
-        }
-        else if(this.checkIfAnyPlayerHasTheAdvantage()){
-            score=this.calculateResultForWhichPlayerHasTheAdvantage();
-        }
-        else{
-            score=this.calculateScoreIfPointsAreNotTied();
+    if (this.checkIfAnyPlayerHasWon()) {
+        score = this.calculateWhichPlayerWon();
+    } else {
+        if (this.checkIfPlayerPointsAreTied()) {
+            score = this.calculateScoreForTiedPoints();
+        } else if (this.checkIfAnyPlayerHasTheAdvantage()) {
+            score = this.calculateResultForWhichPlayerHasTheAdvantage();
+        } else {
+            score = this.calculateScoreIfPointsAreNotTied();
         }
     }
     return score;
 };
 
 
-TennisGame2.prototype.calculateScoreForTiedPoints=function(){
+TennisGame2.prototype.calculateScoreForTiedPoints = function () {
     let result;
     switch (this.Player1CurrentPoints) {
         case 1:
-            result="Fifteen-All";
+            result = "Fifteen-All";
             break;
         case 2:
-            result="Thirty-All";
+            result = "Thirty-All";
             break;
         case 3:
-            result="Deuce";
+            result = "Deuce";
             break;
         default:
-            result="Love-All";
+            result = "Love-All";
             break;
     }
     return result;
 };
 
-TennisGame2.prototype.calculateResultFromTheScore=function(score){
+TennisGame2.prototype.calculateResultFromTheScore = function (score) {
     let result;
     switch (score) {
         case 1:
-            result="Fifteen";
+            result = "Fifteen";
             break;
         case 2:
-            result="Thirty";
+            result = "Thirty";
             break;
         case 3:
-            result="Forty";
+            result = "Forty";
             break;
         default:
-            result="Love";
+            result = "Love";
             break;
     }
     return result;
 };
 
 
-TennisGame2.prototype.checkIfAnyPlayerHasWon=function(){
-    let winner=false;
+TennisGame2.prototype.checkIfAnyPlayerHasWon = function () {
+    let winner = false;
     if (this.Player1CurrentPoints >= 4 && this.checkIfTheDifferenceBetweenPointsIsGreaterOrEqualToTwo()) {
         winner = true;
     }
-    if (this.Player2CurrentPoints >= 4 &&this.checkIfTheDifferenceBetweenPointsIsGreaterOrEqualToTwo()) {
+    if (this.Player2CurrentPoints >= 4 && this.checkIfTheDifferenceBetweenPointsIsGreaterOrEqualToTwo()) {
         winner = true;
     }
     return winner;
 };
 
-TennisGame2.prototype.calculateWhichPlayerWon=function(){
+TennisGame2.prototype.calculateWhichPlayerWon = function () {
     let result;
-    if (this.Player1CurrentPoints >= 4 ) {
+    if (this.Player1CurrentPoints >= 4) {
         result = "Win for player1";
-    }
-   else {
+    } else {
         result = "Win for player2";
     }
     return result;
 };
 
-TennisGame2.prototype.checkIfTheDifferenceBetweenPointsIsGreaterOrEqualToTwo=function () {
-     if(Math.abs(this.Player1CurrentPoints - this.Player2CurrentPoints)>=2)
+TennisGame2.prototype.checkIfTheDifferenceBetweenPointsIsGreaterOrEqualToTwo = function () {
+    if (Math.abs(this.Player1CurrentPoints - this.Player2CurrentPoints) >= 2)
         return true;
     else
         return false;
 };
 
-TennisGame2.prototype.checkIfPlayerPointsAreTied=function(){
-    if (this.Player2CurrentPoints===this.Player1CurrentPoints)
+TennisGame2.prototype.checkIfPlayerPointsAreTied = function () {
+    if (this.Player2CurrentPoints === this.Player1CurrentPoints)
         return true;
-    return false; 
+    return false;
 };
 
 
 
-TennisGame2.prototype.calculateScoreIfPointsAreNotTied = function(){
-    let result1=this.calculateResultFromTheScore(this.Player1CurrentPoints);
-    let result2=this.calculateResultFromTheScore(this.Player2CurrentPoints);
-    return result1+"-"+result2;
+TennisGame2.prototype.calculateScoreIfPointsAreNotTied = function () {
+    let result1 = this.calculateResultFromTheScore(this.Player1CurrentPoints);
+    let result2 = this.calculateResultFromTheScore(this.Player2CurrentPoints);
+    return result1 + "-" + result2;
 };
 
-TennisGame2.prototype.checkIfPlayer1IsWinning = function(){
-    if (this.Player1CurrentPoints>this.Player2CurrentPoints)
+TennisGame2.prototype.checkIfPlayer1IsWinning = function () {
+    if (this.Player1CurrentPoints > this.Player2CurrentPoints)
         return true;
-    return false; 
+    return false;
 };
 
-TennisGame2.prototype.checkIfPlayer2IsWinning = function(){
-    if (this.Player2CurrentPoints>this.Player1CurrentPoints)
+TennisGame2.prototype.checkIfPlayer2IsWinning = function () {
+    if (this.Player2CurrentPoints > this.Player1CurrentPoints)
         return true;
-    return false; 
+    return false;
 };
 
-TennisGame2.prototype.checkIfAnyPlayerHasTheAdvantage = function(){
-    let result=false;
-    if(this.checkIfPlayer1IsWinning()&&this.Player2CurrentPoints >= 3){
+TennisGame2.prototype.checkIfAnyPlayerHasTheAdvantage = function () {
+    let result = false;
+    if (this.checkIfPlayer1IsWinning() && this.Player2CurrentPoints >= 3) {
         result = true;
     }
-    if(this.checkIfPlayer2IsWinning()&&this.Player1CurrentPoints >= 3){
+    if (this.checkIfPlayer2IsWinning() && this.Player1CurrentPoints >= 3) {
         result = true;
     }
     return result;
 };
 
-TennisGame2.prototype.calculateResultForWhichPlayerHasTheAdvantage = function(){
+TennisGame2.prototype.calculateResultForWhichPlayerHasTheAdvantage = function () {
     let advantage;
-    if(this.checkIfPlayer1IsWinning()){
+    if (this.checkIfPlayer1IsWinning()) {
         advantage = "Advantage player1";
     }
-    if(this.checkIfPlayer2IsWinning()){
+    if (this.checkIfPlayer2IsWinning()) {
         advantage = "Advantage player2";
     }
     return advantage;
 };
 
-TennisGame2.prototype.changeP1PointsToAnSpecificValue = function(number) {
-    this.Player1CurrentPoints+=number;
+TennisGame2.prototype.changeP1PointsToAnSpecificValue = function (number) {
+    this.Player1CurrentPoints += number;
 };
 
-TennisGame2.prototype.changeP2PointsToAnSpecificValue = function(number) {
-   this.Player2CurrentPoints+=number;
+TennisGame2.prototype.changeP2PointsToAnSpecificValue = function (number) {
+    this.Player2CurrentPoints += number;
 };
 
-TennisGame2.prototype.increaseP1PointsByOne = function() {
-    this.Player1CurrentPoints++;
-};
-
-TennisGame2.prototype.increaseP2PointsByOne = function() {
-    this.Player2CurrentPoints++;
-};
-
-TennisGame2.prototype.increaseThePointsOfAnyPlayerByOne = function(player) {
-    if (player === "player1")
-        this.increaseP1PointsByOne();
-    else
-        this.increaseP2PointsByOne();
-};
 
 if (typeof window === "undefined") {
     module.exports = TennisGame2;
--- test/tennisGame2_test.js
+++ test/tennisGame2_test.js
@@ -1,114 +1,114 @@
 var assert = require('assert');
 import TennisGame2 from '../TennisGame2'
 describe('TennisGame', function () {
-    let game;   
-    beforeEach(function(){
-        game=new TennisGame2("player1","player2");
+    let game;
+    beforeEach(function () {
+        game = new TennisGame2("player1", "player2");
     });
     it('it should return "Love-All" if no one scored', function () {
-        assert.equal(game.calculateCurrentGameScore(),"Love-All");
+        assert.equal(game.calculateCurrentGameScore(), "Love-All");
     });
     it('It should return "Fifteen-Love" if player1 scored', function () {
-            game.changeP1PointsToAnSpecificValue(1);
-            assert.equal(game.calculateCurrentGameScore(),"Fifteen-Love");
+        game.changeP1PointsToAnSpecificValue(1);
+        assert.equal(game.calculateCurrentGameScore(), "Fifteen-Love");
     });
     it('It should return "Thirty-Love" if player1 scored twice', function () {
         game.changeP1PointsToAnSpecificValue(2);
-        assert.equal(game.calculateCurrentGameScore(),"Thirty-Love");
+        assert.equal(game.calculateCurrentGameScore(), "Thirty-Love");
     });
     it('It should return "Forty-Love" if player1 scored three times', function () {
         game.changeP1PointsToAnSpecificValue(3);
-        assert.equal(game.calculateCurrentGameScore(),"Forty-Love");
+        assert.equal(game.calculateCurrentGameScore(), "Forty-Love");
     });
     it('It should return "Win for player1" if player1 scored four times', function () {
         game.changeP1PointsToAnSpecificValue(4);
-        assert.equal(game.calculateCurrentGameScore(),"Win for player1");
+        assert.equal(game.calculateCurrentGameScore(), "Win for player1");
     });
 
     it('It should return "Love-Fifteen" if player2 scored', function () {
         game.changeP2PointsToAnSpecificValue(1);
-        assert.equal(game.calculateCurrentGameScore(),"Love-Fifteen");
+        assert.equal(game.calculateCurrentGameScore(), "Love-Fifteen");
     });
     it('It should return "Love-Thirty" if player2 scored twice', function () {
         game.changeP2PointsToAnSpecificValue(2);
-        assert.equal(game.calculateCurrentGameScore(),"Love-Thirty");
+        assert.equal(game.calculateCurrentGameScore(), "Love-Thirty");
     });
     it('It should return "Love-Forty" if player2 scored thrice', function () {
         game.changeP2PointsToAnSpecificValue(3);
-        assert.equal(game.calculateCurrentGameScore(),"Love-Forty");
+        assert.equal(game.calculateCurrentGameScore(), "Love-Forty");
     });
     it('It should return "Win for player2" if player2 scored four times', function () {
         game.changeP2PointsToAnSpecificValue(4);
-        assert.equal(game.calculateCurrentGameScore(),"Win for player2");
+        assert.equal(game.calculateCurrentGameScore(), "Win for player2");
     });
 
     it('It should return "Fifteen-All" if both player1 and player2 scored once', function () {
         game.changeP1PointsToAnSpecificValue(1);
         game.changeP2PointsToAnSpecificValue(1);
-        assert.equal(game.calculateCurrentGameScore(),"Fifteen-All");
+        assert.equal(game.calculateCurrentGameScore(), "Fifteen-All");
     });
     it('It should return "Thirty-All" if both player1 and player2 scored twice', function () {
         game.changeP1PointsToAnSpecificValue(2);
         game.changeP2PointsToAnSpecificValue(2);
-        assert.equal(game.calculateCurrentGameScore(),"Thirty-All");
+        assert.equal(game.calculateCurrentGameScore(), "Thirty-All");
     });
     it('It should return "Deuce" if both player1 and player2 scored three times', function () {
         game.changeP1PointsToAnSpecificValue(3);
         game.changeP2PointsToAnSpecificValue(3);
-        assert.equal(game.calculateCurrentGameScore(),"Deuce");
+        assert.equal(game.calculateCurrentGameScore(), "Deuce");
     });
 
     it('It should return "Advantage player1" if player2 current points are "3" or more and player1 just scored to surpass player2 score', function () {
         game.changeP1PointsToAnSpecificValue(4);
         game.changeP2PointsToAnSpecificValue(3);
-        assert.equal(game.calculateCurrentGameScore(),"Advantage player1");
-    });  
+        assert.equal(game.calculateCurrentGameScore(), "Advantage player1");
+    });
     it('It should return "Advantage player2" if player1 current points are "4" or more and player2 just scored to surpass player2 score', function () {
         game.changeP1PointsToAnSpecificValue(3);
         game.changeP2PointsToAnSpecificValue(4);
-        assert.equal(game.calculateCurrentGameScore(),"Advantage player2");
-    });  
+        assert.equal(game.calculateCurrentGameScore(), "Advantage player2");
+    });
 
     it('It should return "Advantage player2" if player1 current points are "4" or more and player2 just scored to surpass player2 score', function () {
         game.changeP1PointsToAnSpecificValue(3);
         game.changeP2PointsToAnSpecificValue(4);
-        assert.equal(game.calculateCurrentGameScore(),"Advantage player2");
-    });  
+        assert.equal(game.calculateCurrentGameScore(), "Advantage player2");
+    });
 
     it('It should return "Fifteen-Thirty" if player2 scored twice and player1 scored once', function () {
         game.changeP2PointsToAnSpecificValue(2);
         game.changeP1PointsToAnSpecificValue(1);
-        assert.equal(game.calculateCurrentGameScore(),"Fifteen-Thirty");
+        assert.equal(game.calculateCurrentGameScore(), "Fifteen-Thirty");
     });
-    
+
     it('It should return "Fifteen-Forty" if player2 scored thrice and player1 scored once', function () {
         game.changeP2PointsToAnSpecificValue(3);
         game.changeP1PointsToAnSpecificValue(1);
-        assert.equal(game.calculateCurrentGameScore(),"Fifteen-Forty");
+        assert.equal(game.calculateCurrentGameScore(), "Fifteen-Forty");
     });
 
     it('It should return "Thirty-Forty" if player2 scored thrice and player1 scored twice', function () {
         game.changeP2PointsToAnSpecificValue(3);
         game.changeP1PointsToAnSpecificValue(2);
-        assert.equal(game.calculateCurrentGameScore(),"Thirty-Forty");
+        assert.equal(game.calculateCurrentGameScore(), "Thirty-Forty");
     });
-    
+
 
     it('It should return "Thirty-Fifteen" if player1 scored twice and player2 scored once', function () {
         game.changeP2PointsToAnSpecificValue(1);
         game.changeP1PointsToAnSpecificValue(2);
-        assert.equal(game.calculateCurrentGameScore(),"Thirty-Fifteen");
+        assert.equal(game.calculateCurrentGameScore(), "Thirty-Fifteen");
     });
-    
+
     it('It should return "Forty-Fifteen" if player1 scored thrice and player2 scored once', function () {
         game.changeP2PointsToAnSpecificValue(1);
         game.changeP1PointsToAnSpecificValue(3);
-        assert.equal(game.calculateCurrentGameScore(),"Forty-Fifteen");
+        assert.equal(game.calculateCurrentGameScore(), "Forty-Fifteen");
     });
 
     it('It should return "Forty-Thirty" if player1 scored thrice and player2 scored twice', function () {
         game.changeP2PointsToAnSpecificValue(2);
         game.changeP1PointsToAnSpecificValue(3);
-        assert.equal(game.calculateCurrentGameScore(),"Forty-Thirty");
+        assert.equal(game.calculateCurrentGameScore(), "Forty-Thirty");
     });
 });
